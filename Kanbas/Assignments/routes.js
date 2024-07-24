9f59f2a76ac466ad75ecd5ef7ea291f474c3236a
Amitabh1998 <amitabh.das1998@gmail.com>
2024-07-24 16:17:28 -0400

    Assignment 5

--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -1,32 +1,33 @@
 import Database from "../Database/index.js";
 export default function AssignmentRoutes(app) {
-    app.post("/api/assignment/:assignmentId", (req, res) => {
-        const { assignmentId } = req.params;
+    app.post("/api/courses/:cid/assignments", (req, res) => {
+        const { cid } = req.params;
         const newAssignment = {
           ...req.body,
-          assignment: assignmentId,
+          course: cid,
+        //   assignment: assignmentId,
           _id: new Date().getTime().toString(),
-        };
-        db.assignments.push(newAssignment);
+        }
+        Database.assignments.push(newAssignment);
         res.send(newAssignment);
       });
     
-  app.get("/api/assignment/assignmentId", (req, res) => {
-    const { assignmentId } = req.params;
-    const ass = db.modules.filter((m) => m.course === cid);
-    res.json(assignment);
+  app.get("/api/courses/:cid/assignments", (req, res) => {
+    const { cid } = req.params;
+    const assignments = Database.assignments.filter((a) => a.course === cid);
+    res.json(assignments);
   });
-  app.delete("/api/courses/:cid/", (req, res) => {
-    const { mid } = req.params;
-    db.modules = db.modules.filter((m) => m._id !== mid);
+  app.delete("/api/assignments/:assignmentId", (req, res) => {
+    const { assignmentId } = req.params;
+    Database.assignments = Database.assignments.filter((a) => a._id !== assignmentId);
     res.sendStatus(200);
   });
-  app.put("/api/modules/:mid", (req, res) => {
-    const { mid } = req.params;
-    const moduleIndex = db.modules.findIndex(
-      (m) => m._id === mid);
-    db.modules[moduleIndex] = {
-      ...db.modules[moduleIndex],
+  app.put("/api/assignments/:assignmentId", (req, res) => {
+    const { assignmentId } = req.params;
+    const assignmentIdx = Database.assignments.findIndex(
+      (a) => a._id === assignmentId);
+      Database.assignments[assignmentIdx] = {
+      ...Database.assignments[assignmentIdx],
       ...req.body
     };
     res.sendStatus(204);
